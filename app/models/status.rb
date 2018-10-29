cfb8dd49a4f5f974d98fe653740e39c15fb3cfff
kyori19 <kyori.dis@gmail.com>
2018-10-29 17:13:31 +0900

    Revert "avatar_emoji"
    This reverts commit 77f8b4f623e861cc8cb0172d00856285e57567e1.

--- app/models/status.rb
+++ app/models/status.rb
@@ -197,10 +197,6 @@ class Status < ApplicationRecord
     @emojis ||= CustomEmoji.from_text([spoiler_text, text].join(' '), account.domain)
   end
 
-  def avatar_emojis
-    AvatarEmoji.from_text([spoiler_text, text].join(' '), account.domain)
-  end
-
   def mark_for_mass_destruction!
     @marked_for_mass_destruction = true
   end
