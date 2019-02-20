2d83e6971ea1dacb6455e874d9e194f84f24ee31
h3zjp <h3zjp@users.noreply.github.com>
2019-02-20 21:42:34 +0900

    文字数制限を2,000文字まで上げる

--- app/models/report_note.rb
+++ app/models/report_note.rb
@@ -17,5 +17,5 @@ class ReportNote < ApplicationRecord
 
   scope :latest, -> { reorder(created_at: :desc) }
 
-  validates :content, presence: true, length: { maximum: 500 }
+  validates :content, presence: true, length: { maximum: 200 }
 end
