21e207a07d89c4c6d5ee9c5270e54f4d22aee033
h3zjp <h3zjp@users.noreply.github.com>
2019-04-28 17:13:20 +0900

    puma の起動オプションを調整

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,10 @@
-threads_count = ENV.fetch('MAX_THREADS') { 5 }.to_i
+threads_count = ENV.fetch('MAX_THREADS') { 4 }.to_i
 threads threads_count, threads_count
 
 if ENV['SOCKET']
   bind 'unix://' + ENV['SOCKET']
 else
-  port ENV.fetch('PORT') { 3000 }
+  bind 'tcp://192.168.10.3:' + ENV.fetch('PORT') { 3000 }
 end
 
 environment ENV.fetch('RAILS_ENV') { 'development' }
