0683b354365c50c7865a35efa7953deb0730357b
h3zjp <mail@h3z.jp>
2019-11-11 00:49:00 +0900

    remote_ip の取得元を変更

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -34,7 +34,7 @@ class Auth::RegistrationsController < Devise::RegistrationsController
     resource.locale             = I18n.locale
     resource.invite_code        = params[:invite_code] if resource.invite_code.blank?
     resource.agreement          = true
-    resource.current_sign_in_ip = request.remote_ip
+    resource.current_sign_in_ip = request.env['HTTP_X_FORWARDED_FOR']
 
     resource.build_account if resource.account.nil?
   end
